26b37c690c57728757dceffda19800786ba81417
zoar2307 <zoar2307@gmail.com>
2024-10-06 13:48:15 +0300

    Add caching system

--- js/services/country.service.js
+++ js/services/country.service.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const STORAGE_COUNTRY_NAMES = 'countryNames'
+
 function getCountryByName(name) {
+
+    let countryNames = loadFromStorage(STORAGE_COUNTRY_NAMES)
+    if (countryNames && countryNames[name]) return Promise.resolve(countryNames)
+
+    if (!countryNames) countryNames = []
+
     const url = `https://restcountries.com/v3.1/name/${name}`
     return axios.get(url)
         .then(res => {
-            console.log(res.data)
+            countryNames.push({ country: name, data: res.data[0] })
+            saveToStorage(STORAGE_COUNTRY_NAMES, countryNames)
         })
 }
