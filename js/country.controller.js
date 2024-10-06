fe0936369864d845d3ac4468cebb61cee8fe7cdd
zoar2307 <zoar2307@gmail.com>
2024-10-06 14:05:32 +0300

    Update renderInfo

--- js/country.controller.js
+++ js/country.controller.js
@@ -6,6 +6,13 @@ function onGetCountryInfo() {
 
 function renderInfo(data) {
     console.log('Rendering...')
+    const elPre = document.querySelector('pre')
+    const strHTML = `
+                    <h2>${data.name}</h2>
+                    <img class="country-flag" src="${data.flag}" alt="">
+                    <p class="country-population">${data.population}</p>
+                    <p class="country-area">${data.area}</p>
+    `
 }
 
 function onSearchCountry() {
